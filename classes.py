edd8d21049b7223c6d57453811508cd78db53a00
captainmeta4 <admin@MAGAbook.local>
2018-09-19 11:49:05 -0400

    fix classes

--- classes.py
+++ classes.py
@@ -1,8 +1,12 @@
 import psycopg2
 import os
+import re
 
 conn=psycopg2.connect(os.environ.get("DATABASE_URL"))
 c=conn.cursor()
+
+#clear any aborted transactions from previous iteration (debugging)
+c.execute("ROLLBACK TRANSACTION")
 
 #prepare parameterized sql statements
 c.execute("PREPARE GetUser(name) AS SELECT * FROM Users WHERE reddit_name=$1")
@@ -10,6 +14,9 @@ c.execute("PREPARE GetUser(name) AS SELECT * FROM Users WHERE reddit_name=$1")
 class User():
 
     def __init__(self, name):
+
+        #sanitize name
+        name=re.search("^[A-Za-z_-]+", name).group[1]
 
         #check database
         c.execute("EXECUTE GetUser({})".format(name))
