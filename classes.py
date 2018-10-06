bc4c255869c57cd0e9d5024d2590a84f2347510e
captainmeta4 <captainmeta4@users.noreply.github.com>
2018-10-06 08:38:12 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -7,65 +7,7 @@ import bleach
 import time
 import signal
 import sys
-
-db=psycopg2.connect(os.environ.get("DATABASE_URL"))
-c=db.cursor()
-
-#wrapper for commit() - make test env read only
-DOMAIN=os.environ.get("domain")
-print(DOMAIN)
-def commit():
-    if DOMAIN=="www.captainslogbook.org":
-        db.commit()
-
-#and rollback transaction on kill if test env
-def signal_term_handler(signal, frame):
-    if DOMAIN!="www.captainslogbook.org":
-        db.rollback()
-    c.close()
-    db.close()
-    sys.exit()
-signal.signal(signal.SIGTERM, signal_term_handler)
-
-#turn off autocommit
-c.execute("SET AUTOCOMMIT=OFF")
-
-#prepare parameterized sql statements
-#for users
-c.execute("PREPARE MakeUser(text) AS INSERT INTO Users (reddit_name, created_utc, google_analytics) VALUES ($1,'NOW','') RETURNING *")
-c.execute("PREPARE GetUserByName(text) AS SELECT * FROM Users WHERE UPPER(reddit_name) = UPPER($1)")
-c.execute("PREPARE GetUserByID(int) AS SELECT * FROM Users WHERE id = $1")
-c.execute("PREPARE BanUser(int, boolean) AS UPDATE Users SET banned=$2 WHERE id=$1")
-c.execute("PREPARE GetUserByToken(text) AS SELECT * FROM Users WHERE token=$1")
-c.execute("PREPARE UpdateToken(int, text) AS UPDATE Users SET token=$2 WHERE id=$1")
-c.execute("PREPARE SetPatreon(int, int, text) AS UPDATE Users SET patreon_id=$2, patreon=$3 WHERE id=$1")
-c.execute("PREPARE SetGoogle(int, text) AS UPDATE Users SET google_analytics=$2 WHERE id=$1")
-c.execute("PREPARE SetOver18(int, boolean) AS UPDATE Users SET over_18=$2 WHERE id=$1")
-c.execute("PREPARE SetPatreonWebhook(int, text) AS UPDATE Users SET patreon_webhook_secret=$2 WHERE id=$1")
-
-#for stories
-c.execute("PREPARE MakeStory(int, text, text, text, text, text, text, text, int) AS INSERT INTO Stories (author_id, created, title, pre, story, post, pre_raw, story_raw, post_raw, book_id) VALUES ($1,'NOW', $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *")
-c.execute("PREPARE EditStory(int, text, text, text, text, text, text, text) AS UPDATE Stories SET pre=$2, story=$3, post=$4, pre_raw=$5, story_raw=$6, post_raw=$7, title=$8, edited='NOW' WHERE id=$1")
-c.execute("PREPARE GetStoryById(int) AS SELECT * FROM Stories WHERE id = $1")
-c.execute("PREPARE GetStoriesByAuthorId(int) AS SELECT * FROM Stories WHERE author_id = $1 ORDER BY id DESC")
-c.execute("PREPARE BanStory(int, boolean) AS UPDATE Stories SET banned=$2 WHERE id=$1")
-c.execute("PREPARE DeleteStory(int, boolean) AS UPDATE Stories SET deleted=$2 WHERE id=$1")
-c.execute("PREPARE GetStoriesByBook(int) AS SELECT * FROM Stories WHERE book_id=$1")
-c.execute("PREPARE SetNSFW(int, boolean) AS UPDATE Stories SET nsfw=$2 WHERE id=$1")
-c.execute("PREPARE SetPatreonThreshold(int,int) AS UPDATE Stories SET patreon_threshold=$2 WHERE id=$1")
-
-#for books
-c.execute("PREPARE MakeBook(text, int, text, text) AS INSERT INTO Books (name, author_id, description, description_raw, timestamp) VALUES ($1, $2, $3, $4, 'NOW') RETURNING *")
-c.execute("PREPARE GetBookById(int) AS SELECT * FROM Books WHERE id=$1")
-c.execute("PREPARE GetBooksByAuthorId(int) AS SELECT * FROM Books WHERE author_id=$1")
-c.execute("PREPARE EditBook(text, text, text, int) AS UPDATE Books SET name=$1, description=$2, description_raw=$3, edited='NOW' WHERE id=$4")
-c.execute("PREPARE BanBook(int, boolean) AS UPDATE Books SET banned=$2 WHERE id=$1")
-c.execute("PREPARE DeleteBook(int, boolean) AS UPDATE Books SET deleted=$2 WHERE id=$1")
-
-#for pledges
-c.execute("PREPARE MakePledge(int,int,int) AS INSERT INTO Pledges (author_patreon_id, supporter_patreon_id, pledge_amount_cents) VALUES ($1, $2, $3) RETURNING *")
-c.execute("PREPARE UpdatePledge(int,int,int) AS UPDATE Pledges SET pledge_amount_cents=$3 WHERE author_patreon_id=$1 AND supporter_patreon_id=$2")
-c.execute("PREPARE GetPledge(int,int) AS SELECT * FROM Pledges WHERE author_patreon_id=$1 AND supporter_patreon_id=$2")
+from db_prep import c, db
 
 #Sanitization object used throughout module
 tags=bleach.sanitizer.ALLOWED_TAGS+['p', 'h1','h2','h3','h4','h5','h6','hr','br','table','tr','th','td','del','thead','tbody','tfoot','pre','div','span','img']
